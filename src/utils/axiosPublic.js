572d665cd0bbecb3897535343a8ec27191803947
mdtajrianrashid <tajrianrashid@gmail.com>
2025-12-18 17:20:46 +0000

    Fixing Deployment issues

--- src/utils/axiosPublic.js
+++ src/utils/axiosPublic.js
@@ -1,3 +1,3 @@
 import axios from "axios";
-const axiosPublic = axios.create({ baseURL: import.meta.env.VITE_API_BASE || "http://localhost:5000/api" });
+const axiosPublic = axios.create({ baseURL: import.meta.env.VITE_API_BASE || "https://mer-nickets-server-full-stack-ticke.vercel.app/api" });
 export default axiosPublic;
